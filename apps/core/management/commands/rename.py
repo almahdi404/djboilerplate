1e45da79aca0bc206dbf0f5152320d29c751da35
DevMahdi404 <mdmahdi404@gmail.com>
2021-05-08 14:28:24 +0600

    custom commands improved

--- apps/core/management/commands/rename.py
+++ apps/core/management/commands/rename.py
@@ -5,7 +5,7 @@ from django.core.management.base import BaseCommand, CommandError
 
 
 class Command(BaseCommand):
-    help = 'Renames the Project'
+    help = "Renames the Project"
 
     def add_arguments(self, parser):
         parser.add_argument('old', nargs='+', type=str, help="current project name")
