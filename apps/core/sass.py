3fe9d2e5f078ce3d1cd86dee70eab197766c19c9
Al Mahdi <68297852+devmahdi404@users.noreply.github.com>
2021-07-26 14:15:52 +0600

    sass feature added to django

---
+++ apps/core/sass.py
@@ -0,0 +1,50 @@
+import os
+import sys
+import glob
+import time
+import sass
+import logging
+import threading
+from django.conf import settings
+from watchdog.observers import Observer
+from watchdog.events import FileModifiedEvent
+
+
+def Compiler():
+    BASE_DIR = str(settings.BASE_DIR)
+    staticFolders = glob.glob(os.path.join(BASE_DIR, "apps", "**", "static"))
+    staticFolders += settings.STATICFILES_DIRS
+
+    if settings.DEBUG:
+        def compiler(path):
+            class Event(FileModifiedEvent):
+                def dispatch(self, event):
+                    filename, extension = os.path.splitext(event.src_path)
+                    if extension == ".scss":
+                        d = os.path.dirname(event.src_path)
+                        time.sleep(0.1)
+                        sass.compile(dirname=(d, d), output_style="expanded")
+
+            logging.basicConfig(level=logging.INFO,
+                                format='%(asctime)s - %(message)s',
+                                datefmt='%Y-%m-%d %H:%M:%S')
+            event_handler = Event(path)
+            observer = Observer()
+            observer.schedule(event_handler, path, recursive=True)
+            observer.start()
+            try:
+                while True:
+                    time.sleep(1)
+            except KeyboardInterrupt:
+                observer.stop()
+            observer.join()
+
+        for d in staticFolders:
+            if os.path.isdir(d):
+                sass.compile(dirname=(d, d), output_style="expanded")
+                thread = threading.Thread(
+                    target=compiler, args=(d,), daemon=True)
+                thread.start()
+    else:
+        for d in staticFolders:
+            sass.compile(dirname=(d, d), output_style="expanded")
