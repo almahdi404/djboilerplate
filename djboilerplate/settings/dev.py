bd22c9035513925eb146cda63ce311165469ccc6
Al Mahdi <68297852+devmahdi404@users.noreply.github.com>
2021-11-08 21:47:17 +0600

    Updated to Django 3.2

--- djboilerplate/settings/dev.py
+++ djboilerplate/settings/dev.py
@@ -8,17 +8,17 @@ ALLOWED_HOSTS = ['*']
 
 
 # Database
-# https://docs.djangoproject.com/en/3.1/ref/settings/#databases
+# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
 
 # Password validation
-# https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
+# https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = []
