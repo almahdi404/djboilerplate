7a741e41bfc8be23c2cbe44d3265c0639c8333e8
Al Mahdi <68297852+almahdi404@users.noreply.github.com>
2023-12-04 13:46:33 +0600

    refac: use f string

--- manage.py
+++ manage.py
@@ -8,9 +8,7 @@ from decouple import config
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault(
-        "DJANGO_SETTINGS_MODULE", "project.settings.{}".format(config("ENV"))
-    )
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", f"project.settings.{config('ENV')}")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
